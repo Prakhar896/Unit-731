73bdb7ad3749461ecef2689ec493066d81140718
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-08-01 10:45:34 +0800

    changed to replaceAll

--- js/submitted.js
+++ js/submitted.js
@@ -1,5 +1,5 @@
 const data = location.search.substring(1, location.search.length).split("&")
 
 var senderName = data[0].split("=")[1]
-senderName = senderName.replace("%20", " ")
+senderName = senderName.replaceAll("%20", " ")
 document.getElementById("thanksLabel").innerText = `Thanks for submitting, ${senderName}!`
